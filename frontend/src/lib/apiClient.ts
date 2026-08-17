12381562b4066c26de59a2f4c8552ec2aae2c5e8
NITHISH S <nithishsenthil2025@gmail.com>
2026-08-17 19:54:45 +0530

    V1 Test

--- frontend/src/lib/apiClient.ts
+++ frontend/src/lib/apiClient.ts
@@ -1,17 +1,27 @@
 import axios from 'axios';
 
 const baseURL = (process.env.NEXT_PUBLIC_API_URL || '').replace(/\/+$/, '');
-const apiClient = axios.create({ baseURL, withCredentials: true, timeout: 20000, headers: { 'Content-Type': 'application/json' } });
 
-apiClient.interceptors.request.use((config) => {
-  if (typeof window !== 'undefined') config.headers.set('X-Client-Timezone', Intl.DateTimeFormat().resolvedOptions().timeZone || 'UTC');
-  return config;
+const apiClient = axios.create({
+  baseURL,
+  withCredentials: true,
+  timeout: 20000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
-apiClient.interceptors.response.use((response) => response, (error) => {
-  const requestId = error?.response?.headers?.['x-request-id'];
-  if (requestId) error.requestId = requestId;
-  return Promise.reject(error);
-});
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const requestId = error?.response?.headers?.['x-request-id'];
+
+    if (requestId) {
+      error.requestId = requestId;
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default apiClient;
